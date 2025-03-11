663d6961eb35f94736b0221be3e3225aee84bc7a
ramesh123-tech <skramesh180@gmail.com>
2025-03-11 15:41:40 +0530

    1st commit

---
+++ src/main/java/contacts/createcontacttest.java
@@ -0,0 +1,12 @@
+package contacts;
+
+import org.testng.annotations.Test;
+
+public class createcontacttest {
+	@Test
+	public void contacttest()
+	{
+		System.out.println("=====createcontacttest===");
+	}
+
+}
